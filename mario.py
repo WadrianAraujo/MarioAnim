1ae84bba5a97f61e0eda0ca17b61679e2f9fe493
Wadrian Araujo de Moura <83624647+wadrian-uea@users.noreply.github.com>
2021-07-05 08:38:09 -0400

    add Game Base

---
+++ mario.py
@@ -0,0 +1,67 @@
+import pygame
+from pygame.locals import *
+
+screen = pygame.display.set_mode((640, 640))
+pygame.display.set_caption("Mario")
+
+UP = 0
+RIGHT = 1
+
+direction = 1
+
+walk = [pygame.image.load("assets/walk1.png"),
+        pygame.image.load("assets/walk2.png"),
+        pygame.image.load("assets/walk3.png")]
+
+walk1 = [pygame.image.load("assets/walk1.png"),
+        pygame.image.load("assets/walk1.png"),
+        pygame.image.load("assets/walk2.png"),
+        pygame.image.load("assets/walk2.png"),
+        pygame.image.load("assets/walk3.png"),
+        pygame.image.load("assets/walk3.png")]
+
+jump = [pygame.image.load("assets/jump1.png"),
+        pygame.image.load("assets/jump2.png"),
+        pygame.image.load("assets/jump1.png")]
+
+cont_image = 0
+
+clock = pygame.time.Clock()
+
+mario = pygame.image.load("assets/walk1.png")
+
+def update():
+    global walk, jump , cont_image, direction
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                direction = UP
+            if event.key == K_RIGHT:
+                direction = RIGHT
+
+
+    cont_image += 1
+    print(cont_image)
+    if cont_image >= 3:
+        cont_image = 0
+
+
+    if direction == RIGHT:
+        mario = walk[cont_image]
+    if direction == UP:
+        mario = jump[cont_image]
+    
+
+    screen.fill((0,0,0))
+    mario = pygame.transform.scale(mario, (400, 400))
+    screen.blit(mario,(100,100))
+    
+    pygame.display.update()
+
+while True:
+    update()
+    clock.tick(7)
