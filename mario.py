1fc5c4c62eeea9c61ec577f0e4449e2e66bc5d28
Wadrian Araujo de Moura <83624647+wadrian-uea@users.noreply.github.com>
2021-07-05 16:44:55 -0400

    final update

--- mario.py
+++ mario.py
@@ -6,10 +6,13 @@ pygame.display.set_caption("Mario")
 
 UP = 0
 RIGHT = 1
-
+LEFT = 2
 direction = 1
 
 walk = [pygame.image.load("assets/walk1.png"),
+        pygame.image.load("assets/walk2.png"),
+        pygame.image.load("assets/walk3.png"),
+        pygame.image.load("assets/walk1.png"),
         pygame.image.load("assets/walk2.png"),
         pygame.image.load("assets/walk3.png")]
 
@@ -21,6 +24,9 @@ walk1 = [pygame.image.load("assets/walk1.png"),
         pygame.image.load("assets/walk3.png")]
 
 jump = [pygame.image.load("assets/jump1.png"),
+        pygame.image.load("assets/jump2.png"),
+        pygame.image.load("assets/jump1.png"),
+        pygame.image.load("assets/jump2.png"),
         pygame.image.load("assets/jump2.png"),
         pygame.image.load("assets/jump1.png")]
 
@@ -42,11 +48,12 @@ def update():
                 direction = UP
             if event.key == K_RIGHT:
                 direction = RIGHT
-
+            if event.key == K_LEFT:
+                direction = LEFT
 
     cont_image += 1
     print(cont_image)
-    if cont_image >= 3:
+    if cont_image >= 6:
         cont_image = 0
 
 
@@ -54,9 +61,11 @@ def update():
         mario = walk[cont_image]
     if direction == UP:
         mario = jump[cont_image]
+    if direction == LEFT:
+        mario = walk1[cont_image]    
     
 
-    screen.fill((0,0,0))
+    screen.fill((255,255,255))
     mario = pygame.transform.scale(mario, (400, 400))
     screen.blit(mario,(100,100))
     
